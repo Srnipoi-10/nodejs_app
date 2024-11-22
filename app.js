a07b93755d9c1259982c7e1a32d222b211dda656
Leo DaVinci <srnipoi.163041.10@gmail.com>
2024-11-22 22:20:42 +0000

    Add greet feature

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+// Главная страница
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Динамическое приветствие
+app.get('/greet/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello, ${name}! Welcome to our site.`);
+});
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
