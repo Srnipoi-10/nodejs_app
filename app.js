6e2b7d93b4f2f3d08e2f9f9fe097038535d5a27a
Leo DaVinci <srnipoi.163041.10@gmail.com>
2024-11-23 00:16:00 +0000

    not bad?

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => res.send('Hello World! Antony dumb! But maybe not so.
 // Динамическое приветствие
 app.get('/greet/:name', (req, res) => {
     const name = req.params.name;
-    res.send(`Hello, ${name}! Welcome to our site.`);
+    res.send(`Hello, ${name}! Welcome to our site. Not bad at all!!!`);
 });
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
